6935eb6468617eca7d5ec4520a24b5e2c2fcb82a
manvargs92 <manvargs@PCMasterRace-Manuel.localdomain>
2022-10-20 22:01:52 -0500

    Declarando un método constructor en Java y JavaScript

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("WRT745", new Account("Andrés Herrera", "AND123000"));
+car.passengers = 4;
+car.printDataCar();
--- Java/Main.java
+++ Java/Main.java
@@ -2,19 +2,19 @@ package Java;
 
 class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = new Car("AMQ123", new Account("Andrés Herrera", "AND123000"));
 
-        car.license = "AMQ123";
-        car.driver = "Andrés Herrera";
+        // car.license = "AMQ123";
+        // car.driver = "Andrés Herrera";
         car.passengers = 4;
 
         // System.out.println("Car licence: " + car.license);
         car.printCarData();
 
-        Car car2 = new Car();
+        Car car2 = new Car("QWE567", new Account("Juana Herrera", "JUA456000"));
 
-        car2.license = "QWE567";
-        car2.driver = "Juana Herrera";
+        // car2.license = "QWE567";
+        // car2.driver = "Juana Herrera";
         car2.passengers = 3;
 
         // System.out.println("Car license: " + car2.license);
