fe129de029bd0902f0e3d0e768002f1d4e4c5d42
manvargs92 <manvargs@PCMasterRace-Manuel.localdomain>
2022-10-20 22:08:50 -0500

    Actualización de código JS a ES6+

--- JS/Car.js
+++ JS/Car.js
@@ -1,12 +1,14 @@
-function Car(license, driver) {
-    this.id;
-    this.license = license;
-    this.driver = driver;
-    this.passengers;
-}
-
-Car.prototype.printDataCar = function () {
-    console.log(this.driver);
-    console.log(this.driver.name);
-    console.log(this.driver.document);
+class Car {
+    constructor(license, driver) {
+        this.id;
+        this.license = license;
+        this.driver = driver;
+        this.passengers;
+    }
+    
+    printDataCar() {
+        console.log(this.driver);
+        console.log(this.driver.name);
+        console.log(this.driver.document);
+    }
 }
--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,3 @@
-var car = new Car("WRT745", new Account("Andrés Herrera", "AND123000"));
+let car = new Car("WRT745", new Account("Andrés Herrera", "AND123000"));
 car.passengers = 4;
 car.printDataCar();
